4eee27f9e8201ebae09c5acd4c01f2fbcd6a03d7
dournesh <dournesh@gmail.com>
2018-06-30 17:31:42 +0300

    remove front-end (moved to admin.formetoo.ssr repository)

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -3,9 +3,9 @@ const AuthProvider = require('../../core/auth.provider')
 module.exports = (app, client) => {
 	let resourceCollection = resource => client.db('testdb').collection(resource)
 
-	app.all('*', (req, res, next) => {
+	app.all('*', async (req, res, next) => {
 		const token = req.headers.authorization
-		const isVerified = AuthProvider._verifyToken(resourceCollection('users'), token)
+		const isVerified = await AuthProvider._verifyToken(resourceCollection('users'), token)
 		if ((!isVerified || !token) && req.params[0] !== '/api/login' && req.params[0].indexOf('import') === -1) {
 			return res
 				.status(401)
--- src/front/configs/config.dev.js
+++
@@ -1,7 +0,0 @@
-export default {
-    uri: {
-        admin: 'http://localhost:3000',
-        login: 'http://localhost:3000/login',
-        allowed: 'http://localhost:3000/allowed'
-    }
-}
