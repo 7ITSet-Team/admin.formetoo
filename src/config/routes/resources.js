675a15d73508bc33ae43ee5f22e1a03c04ca4ba3
Dournesh <Dournesh@gmail.com>
2018-07-04 14:02:20 +0300

    add logs feature

--- src/config/routes/resources.js
+++ src/config/routes/resources.js
@@ -58,7 +58,7 @@ module.exports = (app, resourceCollection) => {
         if (req.params.resource === 'tabs') {
             const tabSets = await resourceCollection('tab-sets').find({
                 slug: {
-                    $in: product['tab-sets']
+                    $in: req.body
                 }
             }).toArray()
             let tabSlugs = []
@@ -81,7 +81,7 @@ module.exports = (app, resourceCollection) => {
             fs.unlinkSync(req.file.path)
             const parsed = Papa.parse(data, {
                 delimiter: ';',
-                encoding: 'CP1251',
+                encoding: 'utf-8',
                 header: true
             })
             const output = parsed.data
@@ -98,11 +98,11 @@ module.exports = (app, resourceCollection) => {
                 delete item.seo_title
                 delete item.seo_description
                 delete item.seo_keywords
-                !!item.categories ? item.categories = item.categories.split(/\s*,\s*/) : item.categories = []
-                !!item['tab-sets'] ? item['tab-sets'] = item['tab-sets'].split(/\s*,\s*/) : item['tab-sets'] = []
-                !!item['attribute-sets'] ? item['attribute-sets'] = item['attribute-sets'].split(/\s*,\s*/) : item['attribute-sets'] = []
-                !!item.images ? item.images = item.images.split(/\s*,\s*/) : item.images = []
-                !!item.relatedProducts ? item.relatedProducts = item.relatedProducts.split(/\s*,\s*/) : item.relatedProducts = []
+                !!item.categories ? item.categories = item.categories.split(' ') : item.categories = []
+                !!item['tab-sets'] ? item['tab-sets'] = item['tab-sets'].split(' ') : item['tab-sets'] = []
+                !!item['attribute-sets'] ? item['attribute-sets'] = item['attribute-sets'].split(' ') : item['attribute-sets'] = []
+                !!item.images ? item.images = item.images.split(' ') : item.images = []
+                !!item.relatedProducts ? item.relatedProducts = item.relatedProducts.split(' ') : item.relatedProducts = []
                 item.attributes = []
                 item.tabs = []
                 item.creationDate = new Date()
@@ -124,7 +124,7 @@ module.exports = (app, resourceCollection) => {
                 ...resource,
                 seo_title: resource.seo.title,
                 seo_description: resource.seo.description,
-                seo_keywords: resource.seo.keywords
+                seo_keywords: resource.seo.keywords.join(' ')
             }
             resource.attributes.forEach(attribute => {
                 if (attribute.attrType === 'multipleSelect') {
@@ -151,16 +151,20 @@ module.exports = (app, resourceCollection) => {
             delete newResource._id
             delete newResource.attributes
             delete newResource.tabs
-            !!newResource.categories ? newResource.categories = newResource.categories.join(', ') : newResource.categories = ''
-            !!newResource['attribute-sets'] ? newResource['attribute-sets'] = newResource['attribute-sets'].join(', ') : newResource['attribute-sets'] = ''
-            !!newResource['tab-sets'] ? newResource['tab-sets'] = newResource['tab-sets'].join(', ') : newResource['tab-sets'] = ''
-            !!newResource.fromSet ? newResource.fromSet = newResource.fromSet.join(', ') : newResource.fromSet = ''
-            !!newResource.relatedProducts ? newResource.relatedProducts = newResource.relatedProducts.join(', ') : newResource.relatedProducts = ''
-            !!newResource.images ? newResource.images = newResource.images.join(', ') : newResource.images = ''
+            !!newResource.categories ? newResource.categories = newResource.categories.join(' ') : newResource.categories = ''
+            !!newResource['attribute-sets'] ? newResource['attribute-sets'] = newResource['attribute-sets'].join(' ') : newResource['attribute-sets'] = ''
+            !!newResource['tab-sets'] ? newResource['tab-sets'] = newResource['tab-sets'].join(' ') : newResource['tab-sets'] = ''
+            !!newResource.fromSet ? newResource.fromSet = newResource.fromSet.join(' ') : newResource.fromSet = ''
+            !!newResource.relatedProducts ? newResource.relatedProducts = newResource.relatedProducts.join(' ') : newResource.relatedProducts = ''
+            !!newResource.images ? newResource.images = newResource.images.join(' ') : newResource.images = ''
+            newResource.creationDate = new Date(newResource.creationDate).toLocaleString()
+            newResource.modificationDate = new Date(newResource.creationDate).toLocaleString()
             return newResource
         })
         const unparse = Papa.unparse(newResources, {
-            encoding: 'utf-8'
+            delimiter: ';',
+            encoding: 'utf-8',
+            header: true
         })
         let time = new Date()
         const formatedDate = time.getHours().toString() + ':' + time.getMinutes().toString() + ':' + time.getSeconds().toString()
@@ -231,7 +235,7 @@ module.exports = (app, resourceCollection) => {
                 msg: `${resource} не найдены!`
             })
         if (resource === 'orders') {
-            resources.map(order => {
+            resources.forEach(order => {
                 let sum = 0
                 order.products.forEach(product => {
                     sum = new Number(sum) + new Number(product.price)
@@ -245,6 +249,11 @@ module.exports = (app, resourceCollection) => {
             }
             return res.send(data)
         }
+        if (resource === 'users' || resource === 'client') {
+            resources.forEach(account => {
+                delete account.password
+            })
+        }
         let data = {
             success: true,
             total: count
@@ -256,12 +265,15 @@ module.exports = (app, resourceCollection) => {
     app.get('/api/:resource/:id', async (req, res) => {
         const resource = req.params.resource
         const resourceItem = await resourceCollection(resource).findOne({_id: ObjectID(req.params.id)})
-        if (!resourceItem)
+        if (!resourceItem) {
             return res.send({
                 success: false,
                 msg: 'Ресурс не найден!'
             })
-
+        }
+        if (resource === 'users' || resource === 'client') {
+            delete resourceItem.password
+        }
         if (resource === 'products') {
             const product = await resourceCollection('products').findOne({_id: ObjectID(req.params.id)})
             const attrSets = await resourceCollection('attribute-sets').find({
@@ -332,22 +344,158 @@ module.exports = (app, resourceCollection) => {
             return res.send(resourceItem)
     })
 
+    app.get('/api/logs/:id/changes', async (req, res) => {
+        const id = req.params.id
+        const log = await resourceCollection('logs').findOne({_id: ObjectID(id)})
+        const before = log.before
+        const after = log.after
+        delete after._id
+        delete before._id
+        let translateKeys = {
+            isActive: 'Активный',
+            title: 'Заголовок',
+            description: 'Описание',
+            shortDescription: 'Краткое описание',
+            sku: 'Артикул',
+            price: 'Цена',
+            categories: 'Категории',
+            'attribute-sets': 'Набор атрибутов',
+            'tab-sets': 'Набор табов',
+            seo: {
+                title: 'СЕО заголовок',
+                description: 'СЕО описание',
+                keywords: 'СЕО ключевые слова'
+            },
+            modificationDate: 'Дата изменения'
+        }
+
+        // МЕТОД СРАВНЕНИЯ ДВУХ ОБЪЕКТОВ
+        Object.prototype.equals = function(object2) {
+            for (propName in this) {
+                if (this.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
+                    return false;
+                }
+                else if (typeof this[propName] != typeof object2[propName]) {
+                    return false;
+                }
+            }
+            for(propName in object2) {
+                if (this.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
+                    return false
+                }
+                else if (typeof this[propName] != typeof object2[propName]) {
+                    return false
+                }
+                if(!this.hasOwnProperty(propName))
+                    continue
+                if (this[propName] instanceof Array && object2[propName] instanceof Array) {
+                    if (!this[propName].equals(object2[propName]))
+                        return false;
+                }
+                else if (this[propName] instanceof Object && object2[propName] instanceof Object) {
+                    if (!this[propName].equals(object2[propName]))
+                        return false;
+                }
+                else if(this[propName] != object2[propName]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // МЕТОД СРАВНЕНИЯ ДВУХ МАССИВОВ
+        Array.prototype.equals = function (array) {
+            if (!array)
+                return false
+            if (this.length != array.length)
+                return false
+            for (var i = 0, l=this.length; i < l; i++) {
+                if (this[i] instanceof Array && array[i] instanceof Array) {
+                    if (!this[i].equals(array[i]))
+                        return false
+                }
+                else if (this[i] instanceof Object && array[i] instanceof Object) {
+                    if (!this[i].equals(array[i]))
+                        return false
+                }
+                else if (this[i] != array[i]) {
+                    return false
+                }
+            }
+            return true
+        }
+        Object.defineProperty(Array.prototype, "equals", {enumerable: false})
+
+        let changes = []
+
+        delete after.attributes
+        delete after.tabs
+        delete before.attributes
+        delete before.tabs
+
+        Object.keys(after).forEach(key => {
+            if (before[key] !== after[key]) {
+                if (typeof before[key] === 'boolean' && typeof after[key] === 'boolean') {
+                    changes.push({
+                        title: translateKeys[key],
+                        before: before[key] === true ? 'Да' : 'Нет',
+                        after: after[key] === true ? 'Да' : 'Нет'
+                    })
+                    return
+                }
+                if (before[key] instanceof Array && after[key] instanceof Array) {
+                    if (!before[key].equals(after[key])) {
+                        changes.push({
+                            title: translateKeys[key],
+                            before: before[key] instanceof Array ? before[key].join(', ') : before[key],
+                            after: after[key] instanceof Array ? after[key].join(', ') : after[key]
+                        })
+                    }
+                    return
+                }
+                if (before[key] instanceof Object && after[key] instanceof Object) {
+                    if (!before[key].equals(after[key])) {
+                        Object.keys(before[key]).forEach(beforeKey => {
+                            if (before[key][beforeKey] !== after[key][beforeKey]) {
+                                changes.push({
+                                    title: translateKeys[key][beforeKey],
+                                    before: before[key][beforeKey] instanceof Array ? before[key][beforeKey].join(', ') : before[key][beforeKey],
+                                    after: after[key][beforeKey] instanceof Array ? after[key][beforeKey].join(', ') : after[key][beforeKey]
+                                })
+                            }
+                        })
+                    }
+                    return
+                }
+                changes.push({
+                    title: translateKeys[key],
+                    before: before[key],
+                    after: after[key]
+                })
+            }
+        })
+
+        res.send(changes)
+    })
+
     app.post('/api/:resource', async (req, res) => {
         const resource = req.params.resource
+        let resourceItem = req.body
         if (resource === 'users') {
-            let user = req.body
-            user.password = await AuthProvider.getHash(req.body.password)
+            let user = resourceItem
+            user.email = user.email.toLowerCase()
+            user.password = await AuthProvider.getHash(resourceItem.password)
             try {
                 await resourceCollection(resource).insert(user)
-                return res.send({
-                    success: true
-                })
             } catch (error) {
                 return res.send({
                     success: false,
                     msg: 'Ошибка создания пользователя'
                 })
             }
+            return res.send({
+                success: true
+            })
         }
         if (resource === 'products') {
             const user = AuthProvider.decode(req.headers.authorization).email
@@ -368,9 +516,9 @@ module.exports = (app, resourceCollection) => {
             const formatedDate = `${hour}:${minutes}:${seconds}`
             try {
                 await resourceCollection(resource).insert({
-                    ...req.body,
-                    attributes: req.body.attributes || [],
-                    tabs: req.body.tabs || []
+                    ...resourceItem,
+                    attributes: resourceItem.attributes || [],
+                    tabs: resourceItem.tabs || []
                 })
             } catch (e) {
                 return res.send({
@@ -383,7 +531,7 @@ module.exports = (app, resourceCollection) => {
                 await resourceCollection('logs').insert({
                     user: user,
                     time: `${time.getDay()} ${month[time.getMonth()]} ${formatedDate}`,
-                    action: `Создал продукт: ${req.body.title}`
+                    action: `Создал продукт: ${resourceItem.title}`
                 })
             } catch (e) {
                 return res.send({
@@ -396,17 +544,20 @@ module.exports = (app, resourceCollection) => {
                 success: true
             })
         }
+        if (resource === 'categories') {
+            resourceItem.seo.keywords = resourceItem.seo.keywords.split(', ')
+        }
         try {
-            await resourceCollection(resource).insert(req.body)
-            return res.send({
-                success: true
-            })
+            await resourceCollection(resource).insert(resourceItem)
         } catch (err) {
             return res.send({
                 success: false,
                 msg: 'Ошибка создания пользователя'
             })
         }
+        res.send({
+            success: true
+        })
     })
 
     app.post('/api/:resource/:id', async (req, res) => {
@@ -426,6 +577,8 @@ module.exports = (app, resourceCollection) => {
                 success: true
             })
         }
+        let newResource = req.body
+        newResource._id = ObjectID(newResource._id)
         if (resource === 'products') {
             const user = AuthProvider.decode(req.headers.authorization).email
             const time = new Date()
@@ -443,11 +596,24 @@ module.exports = (app, resourceCollection) => {
                 seconds = '0' + seconds
             }
             const formatedDate = `${hour}:${minutes}:${seconds}`
+            const beforeChange = await resourceCollection(resource).findOne({_id: ObjectID(req.params.id)})
+            try {
+                await resourceCollection(resource).findOneAndUpdate({_id: ObjectID(req.params.id)}, newResource)
+            } catch (e) {
+                return res.send({
+                    success: false,
+                    msg: 'Ошибка при редактировании продуктов',
+                    error: e
+                })
+            }
+            const afterChange = await resourceCollection(resource).findOne({_id: ObjectID(req.params.id)})
             try {
                 await resourceCollection('logs').insert({
                     user: user,
                     time: `${time.getDay()} ${month[time.getMonth()]} ${formatedDate}`,
-                    action: `Редактировал продукт: ${req.body.title}`
+                    action: `Редактировал продукт: ${req.body.title}`,
+                    before: beforeChange,
+                    after: afterChange
                 })
             } catch (e) {
                 return res.send({
@@ -457,8 +623,6 @@ module.exports = (app, resourceCollection) => {
                 })
             }
         }
-        let newResource = req.body
-        newResource._id = ObjectID(newResource._id)
         await resourceCollection(resource).findOneAndUpdate({_id: ObjectID(req.params.id)}, newResource)
             .catch(() => {
                 return res.send({
